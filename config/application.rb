5ab491dec317d1e55744c550fb9cdc52964ce663
d <derekodai@gmail.com>
2019-01-24 22:14:46 -0500

    ???????

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ module Hungr
     config.assets.initialize_on_precompile = false
     config.assets.enabled = true
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-    config.assets.precompile << /\.(?:svg|eot|woff|ttf|otf)$/
  
 
 
